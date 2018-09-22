de95a80159c62e91dd90fb803a5b140e1f83f6a1
iain2k <iain_allan@mac.com>
2018-09-22 20:13:20 +0100

    Update to process XML

--- process_xml.py
+++ process_xml.py
@@ -2,15 +2,46 @@
 
 
 from xml.etree import ElementTree
+import collections
 import os
 
+Course = collections.namedtuple("Course", "title room building")
+
+
 def main():
-    folder = os.path.basename(__file__)
-    print('Folder == {}'.format(folder))
+    folder = os.path.dirname(__file__)
+    print("Folder == {}".format(folder))
 
-    file = os.path.join(folder, 'xml_data', 'reed.xml')
-    print('File == {}'.format(file))
+    file = os.path.join(folder, "xml_data", "reed.xml")
+    print("File == {}".format(file))
+
+    with open(file) as fin:
+        xml_text = fin.read()
+
+    dom = ElementTree.fromstring(xml_text)
+    # print(dom)
+
+    # courses = dom.findall('course')
+    course_nodes = dom.findall("course")
+
+    courses = []
+    for n in course_nodes:
+        c = Course(
+            n.find("title").text,
+            n.find("place/room").text,
+            n.find("place/building").text,
+        )
+        courses.append(c)
+
+    building = input("What building are you in? ")
+    room = input("What room are you in?")
+
+    room_course = [
+        c.title for c in courses if c.building == building and c.room == room
+    ]
+
+    print(room_course)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
